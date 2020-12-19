e157f10751a0f29a2418a68e446e1a707664d44d
Narasimha <narasimhav@virtuositycg.com>
2020-12-19 20:24:46 +0530

    initial commit

---
+++ client/src/Sample.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Sample = () => {
+	return (
+		<div>
+			Sample Page
+			<Link to='/'>Home</Link>
+		</div>
+	);
+};
+
+export default Sample;
